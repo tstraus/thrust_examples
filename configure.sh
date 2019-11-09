60a14f19a5250d28cc28e67fb56d931b38ccdd0c
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2019-11-09 13:02:32 -0500

    change output dir

--- configure.sh
+++ configure.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-mkdir build
+BUILD_TYPE='Release'
+
+if [[ $1 == '-d' || $1 == '--debug' ]]; then
+    BUILD_TYPE='Debug'
+fi
+
+if [ ! -d build ]; then
+    mkdir build
+fi
+
 cd build
 
-cmake -D CUDA_TOOLKIT_ROOT_DIR=/usr ..
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCUDA_TOOLKIT_ROOT_DIR=/usr -DCMAKE_INSTALL_PREFIX:PATH=dist ..
